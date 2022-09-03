e241b6a15e6ce3f008a30f0a19e6cc23e7da80d1
Aniket <aniketsatarkar9@gmail.com>
2022-09-03 14:47:21 +0530

    first change

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Vue.js with Github CI/CD"/>
   </div>
 </template>
 
